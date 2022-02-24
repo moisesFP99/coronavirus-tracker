60b0a84ae1ebe841e4024c3382379101d08dc190
Moises <moisesxd8@gmail.com>
2022-02-24 20:27:11 -0300

    project done

--- src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
+++ src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
@@ -14,14 +14,16 @@ import io.javabrains.coronavirustracker.services.CoronaVirusDataService;
 public class HomeController {
 	
 	@Autowired
-	CoronaVirusDataService coronaVirusVataService;
+	CoronaVirusDataService coronaVirusDataService;
 	
 	@GetMapping("/")
 	public String home(Model model) {
-		List<LocationStats> allStats = coronaVirusVataService.getAllStats();
+		List<LocationStats> allStats = coronaVirusDataService.getAllStats();
 		int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+		int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
 		model.addAttribute("locationStats", allStats);
 		model.addAttribute("totalReportedCases", totalReportedCases);
+		model.addAttribute("totalNewCases", totalNewCases);
 		
 		return "home";
 	}
